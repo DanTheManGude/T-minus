64ef6765b4a25e2ae326e8b52fb0c2b5ea7caa1d
DanTheManGude <dgude31@gmail.com>
2024-03-31 16:38:12 -0400

    set fontsize from text area height

--- src/Components/MainTask.js
+++ src/Components/MainTask.js
@@ -1,28 +1,18 @@
 import { useEffect, useRef } from "react";
 
-const textAreaId = "mainTaskInput";
-
-function resizeTextArea(element) {
-  element.style.height = "auto";
-  element.style.height = `${element.scrollHeight}px`;
-}
-
 function MainTask(props) {
   const { mainTask, enterMainTask } = props;
 
   const textAreaRef = useRef(null);
 
   useEffect(() => {
-    const element = textAreaRef.current;
-    if (element !== null) {
-      resizeTextArea(element);
-    }
-  }, []);
-
-  useEffect(() => {
     if (window.ResizeObserver) {
       const observer = new ResizeObserver(() => {
-        console.log(textAreaRef.current.style.height);
+        const textAreaHeight = textAreaRef.current.style.height;
+        if (textAreaHeight) {
+          const fontSize = parseInt(textAreaHeight) - 12;
+          textAreaRef.current.style.fontSize = `${fontSize}px`;
+        }
       });
 
       observer.observe(textAreaRef.current);
@@ -39,13 +29,12 @@ function MainTask(props) {
   const updateMainTask = (event) => {
     const element = event.target;
     enterMainTask(element.value);
-    resizeTextArea(element);
   };
 
   return (
     <div>
       <textarea
-        id={textAreaId}
+        id="mainTaskInput"
         ref={textAreaRef}
         type="text"
         value={mainTask}
