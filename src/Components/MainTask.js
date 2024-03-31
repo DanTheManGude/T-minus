8ea9ca45c27d44c4d6c4d5552e095a0982cbd4db
DanTheManGude <dgude31@gmail.com>
2024-03-30 23:15:04 -0400

    rezise text area height

--- src/Components/MainTask.js
+++ src/Components/MainTask.js
@@ -1,19 +1,37 @@
+import { useEffect } from "react";
+
+const textAreaId = "mainTaskInput";
+
+function resizeTextArea(element) {
+  element.style.height = "auto";
+  element.style.height = `${element.scrollHeight}px`;
+}
+
 function MainTask(props) {
   const { mainTask, enterMainTask } = props;
 
+  useEffect(() => {
+    const element = document.getElementById(textAreaId);
+    if (element !== null) {
+      resizeTextArea(element);
+    }
+  });
+
   const updateMainTask = (event) => {
-    const newMainTask = event.target.value;
-    enterMainTask(newMainTask);
+    const element = event.target;
+    enterMainTask(element.value);
+    resizeTextArea(element);
   };
 
   return (
     <div>
       <textarea
-        className="mainTaskInput"
+        id={textAreaId}
         type="text"
         value={mainTask}
         autoComplete="false"
         onChange={updateMainTask}
+        rows={1}
       />
     </div>
   );
